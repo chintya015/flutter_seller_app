1b821cab81674034dd66b478d8a053833a93e1e8
chintya015 <chintyafitriyani02@gmail.com>
2023-12-27 18:36:28 +0700

    update fcm token

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://8c35-2404-8000-1028-1268-a9d6-ad1c-26e3-647c.ngrok-free.app';
+  static const baseUrl = 'https://8c35-2404-8000-1028-1268-a9d6-ad1c-26e3-647c.ngrok-free.appgut';
 }
